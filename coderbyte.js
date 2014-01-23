3fc8b8cbf668480fb2eec6fa8d9d4d97790b6f0b
Cho Kim <4cho.kim@gmail.com>
2014-01-23 12:33:19 -0500

    code now uses function statements

--- coderbyte.js
+++ coderbyte.js
@@ -1,139 +1,139 @@
-var coderbyte = {
-  reverseString: function(string) {
+var coderbyte = {}; 
 
-  	// confirm a string is being passed
-    var stringReversed = "",
-        i, 
-        len; 
+coderbyte.reverseString = function(string) {
 
-    for (i = 0, len = string.length; i < len; i++) {
-      stringReversed = string[i] + stringReversed;
-    }
+	// confirm a string is being passed
+  var stringReversed = "",
+      i, 
+      len; 
 
-    return stringReversed;
-  },
-
-  getFactorial: function(number) {
-
-  	// confirm that a num and a num > 0 is passed
-    var product = 1, 
-        i,
-        len; 
-
-    for (i = number; i > 1 ; i--) {
-      console.log(product = product * i);
-    }
-
-  	return product;
-  },
-
-  getLongestWord: function(sentence) {
-
-  	// confirm that a string was passed  	
-    var santizedSentence = sentence.replace(/[^a-z\s]/gi, ""),
-        words            = santizedSentence.split(" "),
-        longestWord      = "",
-        i, 
-        len;
-
-    for (i = 0, len = words.length; i < len; i++) {
-      if ( words[i].length > longestWord.length) {
-      	longestWord = words[i];
-      }
-    }
-
-  	return longestWord;
-  },
-
-  changeLetters: function(string) {
-
-  	// confirm that a string was passed
-
-
-  },
-
-  addNumbers: function(number) {
- 
-  	// confirm that a number was passed && number is greater than 0
-  	var sum = 0,
-        i;
-
-    for (i = number; i > 0; i--) {
-    	sum += i; 
-    }
-
-    return sum; 
-  },
-
-  capitalizeLetters: function(sentence) {
-
-  	// confirm that a string was passed
-    var words = sentence.split(" "),
-        sentenceCapitalized = "",
-        i,
-        len; 
-
-    for (i = 0, len = words.length; i < length; i++) {
-      sentenceCapitalized += words[i].charAt(0).toUpperCase();
-    };    
-
-  	return sentenceCapitalized;
-  },
-
-  //breaked
-
-  compareNumbers: function(number1, number2) {
-
-  	// confirm that both arguments are numbers
-
-  	if (number1 < number2) {
-  		return true;
-  	} else if ( number1 > number2) {
-  		return false; 
-  	} else {
-  		return -1;
-  	}
-    
-  },
-
-  countVowels: function(string) {
-
-  	//confirm a string was passed
-
-    var regex          = /[^aeiou]/ig,
-        removeChar     = "",
-        vowelsAsString = string.replace(regex, removeChar),
-        vowelCount     = vowelsAsString.length; 
-
-    return vowelCount;
-  },
-
-  insertDashes: function(number) {
-
-  	// confirm that a number was passed
-    var numberAsString = number.toString(),
-        regex          = /([13579])(?=[13579])/g,
-        dash           = "$1-", 
-        numberWithDash = numberAsString.replace(regex, dash);
-    
-    return numberWithDash;
-  },
-
-  addNumbersAsString: function(string) {
-
-  	// should refactor && confirm  that a string was passed
-  	var regex                  = /[^\d]+/g,
-  	    space                  = " ", 
-  	    numbersSpacedDelimited = string.replace(regex, space),
-  	    numbers                = numbersSpacedDelimited.split(space),
-  	    sum                    = 0,
-  	    i,
-  	    len;
-
-    for (i = 0, len = numbers.length; i < len; i++) {
-    	sum += parseInt(numbers[i]);
-    } 
-
-    return sum; 
+  for (i = 0, len = string.length; i < len; i++) {
+    stringReversed = string[i] + stringReversed;
   }
+
+  return stringReversed;
+};
+
+coderbyte.getFactorial = function(number) {
+
+	// confirm that a num and a num > 0 is passed
+  var product = 1, 
+      i,
+      len; 
+
+  for (i = number; i > 1 ; i--) {
+    console.log(product = product * i);
+  }
+
+	return product;
+};
+
+coderbyte.getLongestWord = function(sentence) {
+
+	// confirm that a string was passed  	
+  var santizedSentence = sentence.replace(/[^a-z\s]/gi, ""),
+      words            = santizedSentence.split(" "),
+      longestWord      = "",
+      i, 
+      len;
+
+  for (i = 0, len = words.length; i < len; i++) {
+    if ( words[i].length > longestWord.length) {
+    	longestWord = words[i];
+    }
+  }
+
+	return longestWord;
+};
+
+coderbyte.changeLetters = function(string) {
+
+	// confirm that a string was passed
+
+
+};
+
+coderbyte.addNumbers  = function(number) {
+
+	// confirm that a number was passed && number is greater than 0
+	var sum = 0,
+      i;
+
+  for (i = number; i > 0; i--) {
+  	sum += i; 
+  }
+
+  return sum; 
+};
+
+coderbyte.capitalizeLetters = function(sentence) {
+
+	// confirm that a string was passed
+  var words = sentence.split(" "),
+      sentenceCapitalized = "",
+      i,
+      len; 
+
+  for (i = 0, len = words.length; i < length; i++) {
+    sentenceCapitalized += words[i].charAt(0).toUpperCase();
+  };    
+
+	return sentenceCapitalized;
+};
+
+  //////////////////////////breaked
+
+coderbyte.compareNumbers = function(number1, number2) {
+
+	// confirm that both arguments are numbers
+
+	if (number1 < number2) {
+		return true;
+	} else if ( number1 > number2) {
+		return false; 
+	} else {
+		return -1;
+	}
+  
+};
+
+coderbyte.countVowels = function(string) {
+
+	//confirm a string was passed
+
+  var regex          = /[^aeiou]/ig,
+      removeChar     = "",
+      vowelsAsString = string.replace(regex, removeChar),
+      vowelCount     = vowelsAsString.length; 
+
+  return vowelCount;
+};
+
+coderbyte.insertDashes = function(number) {
+
+	// confirm that a number was passed
+  var numberAsString = number.toString(),
+      regex          = /([13579])(?=[13579])/g,
+      dash           = "$1-", 
+      numberWithDash = numberAsString.replace(regex, dash);
+  
+  return numberWithDash;
+};
+
+coderbyte.addNumbersAsString = function(string) {
+
+	// should refactor && confirm  that a string was passed
+	var regex                  = /[^\d]+/g,
+	    space                  = " ", 
+	    numbersSpacedDelimited = string.replace(regex, space),
+	    numbers                = numbersSpacedDelimited.split(space),
+	    sum                    = 0,
+	    i,
+	    len;
+
+  for (i = 0, len = numbers.length; i < len; i++) {
+  	sum += parseInt(numbers[i]);
+  } 
+
+  return sum; 
 };
